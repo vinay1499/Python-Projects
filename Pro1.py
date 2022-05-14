f84c356d2b83fab890d88356fa0007a7b6c94863
SAI VINAY NANDIGAM <56666558+vinay1499@users.noreply.github.com>
2022-05-14 18:06:17 -0400

    Add files via upload

---
+++ Pro1.py
@@ -0,0 +1,22 @@
+#Madlibs - Project
+# Using Strin Concatenation - aka- How to put strings together
+
+#blank_val=input("Enter the name of the youtuber:")
+
+#  few ways to do this
+# print("Subscribe to "+blank_val)
+# print("Subscribe to {}".format(blank_val))
+# print(f"Subscribe to {blank_val}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famousperson = input("Famous person: ")
+
+madlib = f"Computer Programming is so {adj}! It makes me so excited all the time because I love to {verb1}.\
+ Stay hyderated and {verb2} like you are {famousperson}! "
+
+print(madlib)
+
+
+
